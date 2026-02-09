a493da8561e8c053bf05c1fbbb3357832b421aec
PresidentPlant <jaewonchung7@snu.ac.kr>
2026-02-09 20:21:55 +0900

    Updated gstreamer pipelines
    Signed-off-by: PresidentPlant <jaewonchung7@snu.ac.kr>

--- tools/cameraStream/receive.sh
+++ tools/cameraStream/receive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gst-launch-1.0 srtsrc uri=srt://:5000?mode=listener latency=0 ! \
+gst-launch-1.0 srtsrc uri=srt://:5000 mode=listener keep-listening=true latency=150 ! \
   h265parse ! avdec_h265 ! videoconvert ! autovideosink sync=false
