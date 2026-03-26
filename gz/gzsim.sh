52799e7960be5b82a191f45166480fa8f500f23d
PresidentPlant <jaewonchung7@snu.ac.kr>
2026-03-27 08:45:36 +0900

    Somewhat finished setting up gazebo sim
    Signed-off-by: PresidentPlant <jaewonchung7@snu.ac.kr>

---
+++ gz/gzsim.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gz sim -r rac2026_world.sdf
---
+++ gz/px4sitl.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+PX4_GZ_STANDALONE=1 PX4_SYS_AUTOSTART=4004 PX4_SIM_MODEL=rac2026_vtol /app/PX4-Autopilot/build/px4_sitl_default/bin/px4
